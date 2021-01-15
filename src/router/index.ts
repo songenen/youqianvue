8e0c17519a1f2e58f9065f6128f65f5bc7a9d917
Songsong <614750803@qq.com>
2021-01-15 13:57:09 +0800

    加入404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue'
+import NotFount from '@/views/NotFount.vue'
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,10 @@ const routes = [
   {
     path:'/statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFount
   }
   // {
   //   path: '/',
---
+++ src/views/NotFount.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    404
+    <div>
+      <router-link to="/">返回首页</router-link>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "NotFount"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
