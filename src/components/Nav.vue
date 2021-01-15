dbdb23345fb4ad5ae0d0ea8f8148bb3a9650bef1
Songsong <614750803@qq.com>
2021-01-15 16:07:55 +0800

    添加active-class 点击颜色

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,13 @@
 <template>
     <nav>
-      <router-link to="/money" class="item">
+      <router-link to="/money" class="item" active-class="selected">
         <Icon name="money"/>
         记账
       </router-link>
-      <router-link to="/labels" class="item">
+      <router-link to="/labels" class="item" active-class="selected">
         <Icon name="labels"/>
         标签</router-link>
-      <router-link to="/statistics" class="item">
+      <router-link to="/statistics" class="item" active-class="selected">
         <Icon name="statistics"/>
         统计</router-link>
     </nav>
@@ -36,6 +36,10 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  > .item.selected{
+    color: green;
+
   }
 }
 </style>
