c51135599728f99a39873f37deb5fb151382159c
Songsong <614750803@qq.com>
2021-01-20 22:24:33 +0800

    添加recordAt 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,18 +18,23 @@ import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
+
 type Record = {
   tags: string[];
   notes: string;
   type: string;
-  amount: number;
+  amount: number; // 数据类型 object | string
+  createdAt?: Date;  // 类 / 构造函数
 }
+
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '彩票'];
-  recordList: Record[] = [];
+  recordList: Record[] = recordList;
 
   record: Record = {
     tags: [], notes: '', type: '-', amount: 0
@@ -43,9 +48,9 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
     this.recordList.push(record2);
-    console.log(this.recordList);
   }
   @Watch('recordList')
   onRecordListChange() {
